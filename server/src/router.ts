da592129b59cac669e22f21fb964e64213f282a6
frostzt <aidenfrostbite@gmail.com>
2021-05-05 17:59:30 +0530

    Implementing routing for fetching images from the frontend

--- server/src/router.ts
+++ server/src/router.ts
@@ -4,6 +4,7 @@ const controller = require("./controller");
 
 const router = Router();
 
+router.get("/images");
 router.post("/upload", controller.uploadImage, controller.manipulateImage);
 
 export default router;
