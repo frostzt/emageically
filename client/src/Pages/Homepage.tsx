0c25dda02f427028e66dc8e752bed4ef8762cc80
frostzt <aidenfrostbite@gmail.com>
2021-05-05 21:00:25 +0530

    Implemented the frontend image fetching and started displaying them on the main homepage

--- client/src/Pages/Homepage.tsx
+++ client/src/Pages/Homepage.tsx
@@ -1,4 +1,5 @@
-import React, { Fragment, useState } from "react";
+import React, { Fragment, useState, useEffect } from "react";
+import axios from "axios";
 
 // Style
 import classes from "./Homepage.module.scss";
@@ -10,8 +11,28 @@ import Masonry from "../Layouts/Masonry/Masonry";
 import Header from "../Components/Header/Header";
 import ImageCard from "../Components/ImageCard/ImageCard";
 
+// GLOBALS
+const ENDPOINT = `http://localhost:5000/api/v1/images`;
+
 const Homepage: React.FC = () => {
+  const [length, setLength] = useState(0);
+  const [images, setImages] = useState([]);
   const [search, setSearch] = useState<string>("");
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    try {
+      setIsLoading(true);
+      axios.get(ENDPOINT).then((response) => {
+        setIsLoading(false);
+        setLength(response.data.length);
+        setImages(response.data.formattedFiles);
+      });
+    } catch (error) {
+      console.log(error);
+      setIsLoading(false);
+    }
+  }, []);
 
   // Handle SearchChange
   const handleSearchChange = (e: any) => {
@@ -23,7 +44,19 @@ const Homepage: React.FC = () => {
     <Fragment>
       <Header search={search} handleSearchChange={handleSearchChange} />
       <div className={classes.homepage}>
-        <Masonry></Masonry>
+        {isLoading ? (
+          <h2>Loading...</h2>
+        ) : (
+          <Masonry>
+            {images.map((link, index) => (
+              <ImageCard
+                key={index}
+                imageSource={link}
+                title="Humans are not that good!"
+              />
+            ))}
+          </Masonry>
+        )}
       </div>
     </Fragment>
   );
