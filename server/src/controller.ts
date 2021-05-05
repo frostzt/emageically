2ad8f3d6daada2f4e2d0b6ca416599ff31b0ac3f
frostzt <aidenfrostbite@gmail.com>
2021-05-05 21:11:02 +0530

    Implemented the frontend image fetching and started displaying them on the main homepage

--- server/src/controller.ts
+++ server/src/controller.ts
@@ -55,21 +55,25 @@ exports.manipulateImage = async (req: any, res: Response, _: NextFunction) => {
 exports.getImages = async (req: Request, res: Response, _: NextFunction) => {
   try {
     const files = await readdir(`${__dirname}/public/images`);
-    const formattedFiles: string[] = [];
+    const formattedFiles: Object[] = [];
 
     if (process.env.NODE_ENV === "development") {
       files.forEach((file) =>
-        formattedFiles.push(
-          `${req.protocol + "://" + req.hostname}:5000/images/${file}` // @IMAGES-LINK hardcoded for dev server on PORT=5000
-        )
+        formattedFiles.push({
+          id: nanoid(12),
+          title: file.split(".")[0],
+          link: `${req.protocol + "://" + req.hostname}:5000/images/${file}`,
+        })
       );
-    }
+    } // @IMAGES-LINK hardcoded for dev server on PORT=5000
 
     if (process.env.NODE_ENV === "production") {
       files.forEach((file) =>
-        formattedFiles.push(
-          `${req.protocol + "://" + req.hostname}/images/${file}`
-        )
+        formattedFiles.push({
+          id: nanoid(12),
+          title: file.split(".")[0],
+          link: `${req.protocol + "://" + req.hostname}/images/${file}`,
+        })
       );
     }
 
