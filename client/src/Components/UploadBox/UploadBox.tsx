68eb0bcd9d393e1b47237c97d199b42d7b8bb270
frostzt <aidenfrostbite@gmail.com>
2021-05-09 13:59:48 +0530

    Implementing upload functionality on the frontend

--- client/src/Components/UploadBox/UploadBox.tsx
+++ client/src/Components/UploadBox/UploadBox.tsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 
 // Styling
 import classes from "./UploadBox.module.scss";
+import image from "./ChatEngine.png";
 
 // Components
 import { HTMLButton } from "../Button/Button";
@@ -10,7 +11,15 @@ const UploadBox: React.FC = () => {
   return (
     <div className={classes.uploadBox}>
       <div className={classes.container}>
+        <div className={classes.image}>
+          <img
+            className={classes.image_img}
+            src={image}
+            alt="Preview of uploaded img"
+          />
+        </div>
         <form className={classes.form}>
+          <input className={classes.uploadBtn} required type="file" />
           <HTMLButton>Submit</HTMLButton>
         </form>
       </div>
